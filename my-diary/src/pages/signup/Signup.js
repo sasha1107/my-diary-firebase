d527ab905656c36d15f1df21a6a872a87f4c17a7
Soohyun Jung <sasha1107@naver.com>
2023-02-14 22:09:49 +0900

    Feat: 회원가입 이메일, 비밀번호 유효성 검사

--- my-diary/src/pages/signup/Signup.js
+++ my-diary/src/pages/signup/Signup.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useSignup } from '../../hooks/useSignup'
 import styles from './Signup.module.css'
 
@@ -7,7 +7,10 @@ export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [displayName, setDisplayName] = useState('');
-  const [errMsg, setErrMsg] = useState(null);
+  const [emailErrMsg, setEmailErrMsg] = useState(null);
+  const [pwErrMsg, setPwErrMsg] = useState(null);
+  const emailRef = useRef(null);
+  const pwRef = useRef(null);
   const {error, isPending, signup} = useSignup();
 
   const handleData = (e) => {
@@ -27,9 +30,21 @@ export default function Signup() {
     // console.log(email, password,displayName);
     signup(email, password,displayName)
   }
+  
   useEffect(() => {
     if (error === "Firebase: Error (auth/email-already-in-use)."){
-      setErrMsg("* 이미 가입된 이메일입니다. 다른 이메일 주소를 사용해주세요");
+      setEmailErrMsg("* 이미 가입된 이메일입니다. 다른 이메일 주소를 사용해주세요");
+      setPwErrMsg(null);
+
+      // 이메일 인풋창으로 다시 커서 포커스
+      emailRef.current.focus();
+    }
+    else if (error === "Firebase: Password should be at least 6 characters (auth/weak-password)."){
+      setPwErrMsg("* 비밀번호는 6자 이상 입력해주세요.");
+      setEmailErrMsg(null);
+
+      // 이메일 인풋창으로 다시 커서 포커스
+      pwRef.current.focus();
     }
   }, [error])
 
@@ -39,11 +54,11 @@ export default function Signup() {
         <legend>Signup</legend>
         <label htmlFor='myEmail'>Email</label>
 
-        <input type="email" id="myEmail" required value={email} onChange={handleData}/>
-        <p>{errMsg}</p>
+        <input type="email" id="myEmail" required value={email} ref={emailRef} onChange={handleData}/>
+        <p>{emailErrMsg}</p>
         <label htmlFor='myPassWord'>Password</label>
-        <input type="password" id="myPassWord" required value={password} onChange={handleData}/>
-
+        <input type="password" id="myPassWord" required value={password} ref={pwRef} onChange={handleData}/>
+        <p>{pwErrMsg}</p>
         <label htmlFor='myNickname'>Nickname</label>
         <input type="text" id="myNickname" required value={displayName} onChange={handleData}/>
 
