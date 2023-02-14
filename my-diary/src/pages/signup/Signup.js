68144cae89e5a37470bef8d724fccf82d7ec851b
Soohyun Jung <sasha1107@naver.com>
2023-02-14 21:53:26 +0900

    Feat: 중복 이메일 유효성 검증

--- my-diary/src/pages/signup/Signup.js
+++ my-diary/src/pages/signup/Signup.js
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSignup } from '../../hooks/useSignup'
 import styles from './Signup.module.css'
 
 export default function Signup() {
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [displayName, setDisplayName] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [errMsg, setErrMsg] = useState(null);
   const {error, isPending, signup} = useSignup();
 
   const handleData = (e) => {
@@ -25,15 +26,21 @@ export default function Signup() {
     e.preventDefault(); // 새로고침 막기
     // console.log(email, password,displayName);
     signup(email, password,displayName)
-
   }
+  useEffect(() => {
+    if (error === "Firebase: Error (auth/email-already-in-use)."){
+      setErrMsg("* 이미 가입된 이메일입니다. 다른 이메일 주소를 사용해주세요");
+    }
+  }, [error])
+
   return (
     <form className={styles.signup_form} onSubmit={handleSubmit}>
       <fieldset>
         <legend>Signup</legend>
         <label htmlFor='myEmail'>Email</label>
-        <input type="email" id="myEmail" required value={email} onChange={handleData}/>
 
+        <input type="email" id="myEmail" required value={email} onChange={handleData}/>
+        <p>{errMsg}</p>
         <label htmlFor='myPassWord'>Password</label>
         <input type="password" id="myPassWord" required value={password} onChange={handleData}/>
 
@@ -42,6 +49,8 @@ export default function Signup() {
 
         <button className={styles.btn}>Signup</button>
       </fieldset>
+      
     </form>
+
   )
 }
