59491c74a77cd1cccc5f644a082693ceae6c8291
Soohyun Jung <sasha1107@naver.com>
2023-02-20 13:08:33 +0900

    Fix: Router 분리

--- my-diary/src/App.js
+++ my-diary/src/App.js
@@ -1,15 +1,9 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
-import Nav from './components/Nav';
 import { useAuthContext } from './hooks/useAuthContext';
-import Home from './pages/home/Home'
-import Login from './pages/account/Login'
-import Signup from './pages/account/Signup'
 import { GlobalStyle } from './app.style';
-import Footer from './components/Footer';
 import { FormProvider } from './context/FormContext';
 import { InfoProvider } from './context/InfoContext';
 import Loading from './pages/etc/Loading';
-import Off from './pages/etc/Off';
+import Router from './routes/Router';
 
 function App() {
 
@@ -17,21 +11,12 @@ function App() {
 
   return (
     <div className="App">
-      <GlobalStyle/>
+      <GlobalStyle />
       <FormProvider>
       <InfoProvider>
-      {isAuthReady ? (
-        <BrowserRouter>
-          <Nav />
-          <Routes>
-            <Route path='/' element={user ? <Home /> : <Navigate replace={true} to="/login"></Navigate>}></Route>
-            <Route path='/login' element={!user ? <Login /> : <Navigate replace={true} to="/"></Navigate>}></Route>
-            <Route path='/signup' element={!user ? <Signup /> : <Navigate replace={true} to="/"></Navigate>}></Route>
-            <Route path='/off' element={<Off/>}/>
-          </Routes>
-          <Footer/>
-        </BrowserRouter>
-      ) : <Loading/>}
+        {isAuthReady ? (
+          <Router />
+        ) : <Loading/>}
       </InfoProvider>
       </FormProvider>
     </div >
---
+++ my-diary/src/routes/Router.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
+import Nav from '../components/Nav'
+import Home from '../pages/home/Home'
+import Login from '../pages/account/Login'
+import Signup from '../pages/account/Signup'
+import Off from '../pages/etc/Off'
+import Footer from '../components/Footer'
+export default function Router() {
+    const { isAuthReady, user } = useAuthContext();
+
+    return (
+        <BrowserRouter>
+            <Nav />
+            <Routes>
+                <Route path='/' element={user ? <Home /> : <Navigate replace={true} to="/login"></Navigate>}></Route>
+                <Route path='/login' element={!user ? <Login /> : <Navigate replace={true} to="/"></Navigate>}></Route>
+                <Route path='/signup' element={!user ? <Signup /> : <Navigate replace={true} to="/"></Navigate>}></Route>
+                <Route path='/off' element={<Off/>}/>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
+    )
+}
