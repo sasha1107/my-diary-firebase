c881c0980bf330d2d8a54ea80b98b0cd03db34cf
Soohyun Jung <sasha1107@naver.com>
2023-02-23 11:45:56 +0900

    VAC 패턴 적용(components/DesktopIcon/) #9

---
+++ my-diary/src/components/DesktopIcon/DesktopIconView.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import DragCont from '../DragContainer/DragCont'
+import * as S from "./desktopIcon.style"
+
+    export default function DesktopIconView({ func, src, txt, handleDbClick }) {
+    return (
+        <DragCont>
+            {/* 실행할 함수 func까지 전달 받았다면 실행할 부분 */}
+            {func ? 
+                <S.Container onClick={(e) => handleDbClick(func, e)}>
+                <S.Icon src={src} alt=''/>
+                <S.IconTxt>{txt}</S.IconTxt>
+                </S.Container>
+            : 
+            // 실행할 함수 func 전달 X 시 실행할 부분
+            <S.Container>
+                <S.Icon src={src} alt=''/>
+                <S.IconTxt>{txt}</S.IconTxt>
+            </S.Container>
+            }
+        </DragCont>
+    )
+}
